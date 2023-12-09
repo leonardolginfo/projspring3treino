a07b7005bc571733445c1744e21e8b2f1fde4ee6
leonardolginfo <leonardo@lginfo.com.br>
2023-12-09 11:04:41 -0300

    mudanca b

---
+++ src/test/java/br/com/lginfo/cursospring/CursospringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lginfo.cursospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursospringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
